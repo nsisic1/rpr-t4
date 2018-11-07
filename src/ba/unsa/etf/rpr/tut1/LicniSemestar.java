b328043347c5e46fe8bb1b792814377e260de4cb
Nedim Sisic <nsisic1@etf.unsa.ba>
2018-11-07 23:32:41 +0100

    Dodana klasa LicniSemestar

---
+++ src/ba/unsa/etf/rpr/tut1/LicniSemestar.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr.tut1;
+
+class LicniSemestar extends Semestar {
+    private Semestar planSemestra;
+    private int zbirECTS;
+
+    LicniSemestar() {}
+
+    int getZbirECTSCTS() {
+        return zbirECTS;
+    }
+
+    void upisiPredmet() {}
+    void ispisiPredmet() {}
+}
