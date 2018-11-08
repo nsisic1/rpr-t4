33da37656ee7c71b2240b8a7bc8219e0e426db19
Nedim Sisic <nsisic1@etf.unsa.ba>
2018-11-08 14:00:03 +0100

    Uradjen prvi korak refactoringa, dizajn klasa sa TUTa

--- src/ba/unsa/etf/rpr/tut1/Predmet.java
+++ src/ba/unsa/etf/rpr/tut1/Predmet.java
@@ -1,34 +1,5 @@
 package ba.unsa.etf.rpr.tut1;
 
-import java.util.ArrayList;
-import java.util.List;
+public class Predmet {
 
-class Predmet {
-    private String naziv;
-    private int brojECTS;
-    private boolean obavezan;
-    Semestar semestar;
-    // String profesor;
-
-    Predmet(String naziv, int brojECTS, boolean obavezan, String profesor) {}
-
-    void setNaziv() {}
-    String getNaziv() {
-        return naziv;
-    }
-
-    void setBrojECTS() {}
-    int getBrojECTS() {
-        return brojECTS;
-    }
-
-    void setObavezan() {}
-    boolean isObavezan() {
-        return obavezan;
-    }
-
-    List<Student> dajSpisakStudenata() {
-        System.out.print(this);
-        return Studij.dajStudentePredmeta(this);
-    }
 }
--- src/ba/unsa/etf/rpr/tut1/Student.java
+++ src/ba/unsa/etf/rpr/tut1/Student.java
@@ -1,32 +1,7 @@
 package ba.unsa.etf.rpr.tut1;
 
-class Student {
-    private String ime;
-    private String prezime;
-    private int brojIndeksa;
-    private LicniSemestar semestar;
+public class Student {
 
-    Student(String ime, String prezime, int indeks) {}
+    int brojIndeksa;
 
-    String getIme() {
-        return ime;
-    }
-    void setIme(String ime) {}
-
-    String getPrezime() {
-        return prezime;
-    }
-    void setPrezime(String prezime) {}
-
-    int getBrojIndeksa() {
-        return brojIndeksa;
-    }
-    void setBrojIndeksa(int brojIndeksa) {}
-
-
-
-    void upisi(Predmet predmet) {}
-    void ispisi(Predmet predmet) {}
-    void upisiNoviSemestar() {}
-    void zavrsiStudij() {}
 }
