9a41181429e76523e4df56428f0a0e5ee0287085
Nedim Sisic <nsisic1@etf.unsa.ba>
2018-11-11 17:34:54 +0100

    Enkapsulacija polja klase Student

--- src/ba/unsa/etf/rpr/tut1/Student.java
+++ src/ba/unsa/etf/rpr/tut1/Student.java
@@ -1,7 +1,28 @@
 package ba.unsa.etf.rpr.tut1;
 
-public class Student {
+class Student {
 
-    int brojIndeksa;
+    private String imeIPrezime;
+    private int brojIndeksa;
 
+
+    String getImeIPrezime() {
+        return imeIPrezime;
+    }
+
+    void setImeIPrezime(String imeIPrezime) {
+        this.imeIPrezime = imeIPrezime;
+    }
+
+    int getBrojIndeksa() {
+        return brojIndeksa;
+    }
+
+    void setBrojIndeksa(int brojIndeksa) {
+        this.brojIndeksa = brojIndeksa;
+    }
+
+    void upisiSemestar(int brojSemestra) {
+
+    }
 }
